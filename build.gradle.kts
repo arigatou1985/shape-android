4c3e1508cccaa0bdb346bf844e9ed5b611e6fd48
Jing Yu <jing.yu@electrolux.com>
2025-01-14 11:25:46 +0100

    publishing via maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+group = "com.example"
+version = "1.0.2"
+
 plugins {
 //    id("com.android.library")
 //    kotlin("android")
+    `maven-publish`
+    `java-library`
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
 }
